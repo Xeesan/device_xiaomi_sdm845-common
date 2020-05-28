3010159ef0852813978db262c2c03c5546d5fec5
Martinusbe <martinusbe@gmail.com>
2020-05-28 20:04:40 +0200

    sdm845-common: adapt the exact values for lockscreen charging info
    real time values
    
    Signed-off-by: DennySPB <dennyspb@gmail.com>

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -43,6 +43,9 @@ ifeq ($(TARGET_PREBUILT_KERNEL),)
   TARGET_KERNEL_SOURCE := kernel/xiaomi/sdm845
 endif
 
+# Enable real time lockscreen charging current values
+BOARD_GLOBAL_CFLAGS += -DBATTERY_REAL_INFO
+
 # Platform
 TARGET_BOARD_PLATFORM := sdm845
 TARGET_BOARD_PLATFORM_GPU := qcom-adreno630
