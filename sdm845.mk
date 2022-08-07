f35131890950d95c9a0278b62e1c430925b24363
Dhruvgera <dhruvgera61@gmail.com>
2022-08-07 14:28:40 +0600

    sdm845-commmon: Build sensorservice
    Signed-off-by: Xeesan <ziauddinzisan2001@gmail.com>

--- sdm845.mk
+++ sdm845.mk
@@ -444,6 +444,7 @@ PRODUCT_PACKAGES += \
     android.frameworks.sensorservice@1.0 \
     android.hardware.sensors@1.0-impl:64 \
     android.hardware.sensors@1.0-service \
+    android.frameworks.sensorservice@1.0.vendor \
     libsensorndkbridge
 
 # Soong namespaces
